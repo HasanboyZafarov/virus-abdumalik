30393d68ba16f291a851b277f047b02b76ce9f8a
Hasanboy Zafarov <103839294+HasanboyZafarov@users.noreply.github.com>
2025-08-28 10:15:33 +0500

    Initial project setup with COVID-19 info site
    Added HTML, CSS, and JS files for a COVID-19 information website, including images and assets. Configured VS Code live server settings. The site features sections for overview, contagion, symptoms, prevention, and a contact form.

---
+++ index.js
@@ -0,0 +1,15 @@
+a = prompt(`2.son kiriting`)
+b = prompt(`1.son kiriting`)
+c = prompt(`3.son kiriting`)
+
+ab = a + b 
+ac = a + c 
+bc = b + c
+
+if (ab > ac > bc) {
+    console.log(ab && ac);
+    
+} else {
+    console.log();
+    
+}
